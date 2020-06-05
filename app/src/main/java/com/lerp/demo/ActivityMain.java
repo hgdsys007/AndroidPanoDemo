6ed93eadccbddb1f6e09772a2e05304cf2cd15ea
liupingping <liupingping@fun-snap.com>
2020-06-05 17:39:25 +0800

    切换文件保存目录，解决android 10 创建文件夹失败的问题

--- app/src/main/java/com/lerp/demo/ActivityMain.java
+++ app/src/main/java/com/lerp/demo/ActivityMain.java
@@ -1,11 +1,8 @@
 package com.lerp.demo;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 
 import com.lerp.pano.ImagesStitch;
@@ -17,20 +14,17 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
-import kr.co.namee.permissiongen.PermissionGen;
 
 public class ActivityMain extends AppCompatActivity {
 
-    public static final String DIR = Environment.getExternalStorageDirectory().getPath() + "/360pano/";
-
-    private String[] permissions = {
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE};
+    public static String DIR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DIR = getExternalMediaDirs()[0].getPath() + "/360pano/";
 
         File file = new File(DIR);
         if (!file.exists()) file.mkdirs();
@@ -39,13 +33,6 @@ public class ActivityMain extends AppCompatActivity {
         copyFileFromAssets(this, name, DIR + name);
 
         ImagesStitch.init(this);
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            PermissionGen.with(this)
-                    .addRequestCode(200)
-                    .permissions(permissions)
-                    .request();
-        }
 
 
         findViewById(R.id.btn_vertical).setOnClickListener(new View.OnClickListener() {
